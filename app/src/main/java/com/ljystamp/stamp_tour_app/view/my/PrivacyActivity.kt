72988d74f5d2df8534b503ccb263364324c39a32
idstudent <sm1374@naver.com>
2024-12-25 09:19:02 +0900

    개인정보처리방침 및 앱이름

--- app/src/main/java/com/ljystamp/stamp_tour_app/view/my/PrivacyActivity.kt
+++ app/src/main/java/com/ljystamp/stamp_tour_app/view/my/PrivacyActivity.kt
@@ -2,6 +2,7 @@ package com.ljystamp.stamp_tour_app.view.my
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
 import com.ljystamp.stamp_tour_app.R
 import com.ljystamp.stamp_tour_app.databinding.ActivityPrivacyBinding
 import com.ljystamp.stamp_tour_app.view.BaseActivity
@@ -10,7 +11,15 @@ class PrivacyActivity: BaseActivity<ActivityPrivacyBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // 웹뷰 설정
+        binding.wvPrivacy.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            setSupportZoom(true)
+        }
 
+        binding.wvPrivacy.webViewClient = WebViewClient() // 기본 웹뷰 클라이언트 설정
+        binding.wvPrivacy.loadUrl("https://blog.naver.com/sm1374/223704797394")
     }
 
     override fun getViewBinding(): ActivityPrivacyBinding {
