125e878519667bf700230088c75e91b2756f24c7
idstudent <sm1374@naver.com>
2025-02-06 09:58:54 +0900

    di module작업

--- common/src/main/java/com/ljystamp/common/di/CommonUseCaseModule.kt
+++ common/src/main/java/com/ljystamp/common/di/CommonUseCaseModule.kt
@@ -1,10 +1,32 @@
 package com.ljystamp.common.di
 
+import com.ljystamp.common.domain.respository.GetLocationNearTourListRepository
+import com.ljystamp.common.domain.respository.SaveTourLocationRepository
+import com.ljystamp.common.domain.respository.SavedLocationRepository
+import com.ljystamp.common.domain.usecase.GetLocationNearTourListUseCase
+import com.ljystamp.common.domain.usecase.GetSavedLocationsUseCase
+import com.ljystamp.common.domain.usecase.SaveTourLocationsUseCase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
-@InstallIn
+@InstallIn(SingletonComponent::class)
 class CommonUseCaseModule {
+    @Singleton
+    @Provides
+    fun provideGetLocationNearTourListUseCase(getLocationNearTourListRepository: GetLocationNearTourListRepository) =
+        GetLocationNearTourListUseCase(getLocationNearTourListRepository)
 
+    @Singleton
+    @Provides
+    fun provideGetSavedLocationsUseCase(savedLocationRepository: SavedLocationRepository) =
+        GetSavedLocationsUseCase(savedLocationRepository)
+
+    @Singleton
+    @Provides
+    fun provideSaveTourLocationsUseCase(saveTourLocationRepository: SaveTourLocationRepository) =
+        SaveTourLocationsUseCase(saveTourLocationRepository)
 }
