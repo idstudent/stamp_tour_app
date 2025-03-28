7144a8058ea9cd8c05e4b022dcecc61170275418
idstudent <sm1374@naver.com>
2025-03-28 09:06:51 +0900

    상세페이지 안쓰는 클래스, gradle 삭제

--- feature_search/build.gradle.kts
+++ feature_search/build.gradle.kts
@@ -61,8 +61,7 @@ dependencies {
 
     implementation("com.github.skydoves:sandwich:2.0.5")
     implementation("com.github.skydoves:sandwich-retrofit:2.0.5")
-    
-    implementation("androidx.activity:activity-compose:1.8.2")
+
     implementation("androidx.compose.material3:material3:1.3.1")
 
     implementation("androidx.compose.foundation:foundation:1.5.4")
--- feature_tour_detail/build.gradle.kts
+++ feature_tour_detail/build.gradle.kts
@@ -50,18 +50,10 @@ dependencies {
     implementation(project(":common"))
     implementation(project(":core_navigation"))
 
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.activity:activity-ktx:1.7.2")
-
     implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-android-compiler:2.48")
-    implementation("androidx.hilt:hilt-navigation-fragment:1.2.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
-    implementation("androidx.core:core-ktx:1.9.0")
-
-    implementation("com.github.bumptech.glide:glide:4.14.2")
     implementation("com.github.skydoves:sandwich:2.0.5")
     implementation("com.github.skydoves:sandwich-retrofit:2.0.5")
 
@@ -69,11 +61,8 @@ dependencies {
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
 
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.material3:material3:1.3.1")
 
-    implementation("androidx.compose.foundation:foundation:1.5.4")
     implementation("io.coil-kt:coil-compose:2.5.0")
 }
