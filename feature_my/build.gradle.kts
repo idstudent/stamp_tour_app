0c72cc6008afc22f7e4c906f31f32935248ca2b4
idstudent <sm1374@naver.com>
2025-04-18 09:25:28 +0900

    my모듈 안쓰는 클래스, gradle 삭제

--- feature_my/build.gradle.kts
+++ feature_my/build.gradle.kts
@@ -53,10 +53,6 @@ dependencies {
     implementation(project(":feature_my_tour_detail"))
     implementation(project(":core_navigation"))
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 
     implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
     implementation("com.google.firebase:firebase-auth-ktx")
@@ -64,14 +60,9 @@ dependencies {
 
     implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-android-compiler:2.48")
-    implementation("androidx.hilt:hilt-navigation-fragment:1.2.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
-    implementation("com.github.bumptech.glide:glide:4.14.2")
     implementation("com.google.android.flexbox:flexbox:3.0.0")
-
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.4")
 
     implementation("androidx.compose.material3:material3:1.3.1")
 
