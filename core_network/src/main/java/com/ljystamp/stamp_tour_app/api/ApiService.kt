adc64e6150631dff0425ad370030cf3a0c46a288
idstudent <sm1374@naver.com>
2025-08-11 09:59:45 +0900

    공공데이터 api url 변경으로 인한 수정

--- core_network/src/main/java/com/ljystamp/stamp_tour_app/api/ApiService.kt
+++ core_network/src/main/java/com/ljystamp/stamp_tour_app/api/ApiService.kt
@@ -8,10 +8,10 @@ import retrofit2.http.Query
 
 interface ApiService {
     companion object {
-        val BASE_URL = "https://apis.data.go.kr/B551011/KorService1/"
+        val BASE_URL = "https://apis.data.go.kr/B551011/KorService2/"
     }
 
-    @GET("locationBasedList1")
+    @GET("locationBasedList2")
     suspend fun getLocationTourList(
         @Query("numOfRows") pageResultCount: Int = 20,
         @Query("pageNo") pageNo: Int = 1,
@@ -25,7 +25,7 @@ interface ApiService {
         @Query("contentTypeId") contentTypeId: Int
     ): ApiResponse<TourismResponse?>
 
-    @GET("detailIntro1")
+    @GET("detailIntro2")
     suspend fun getTourDetail(
         @Query("MobileOS") os: String = "AND",
         @Query("MobileApp") mobileOs: String = "TEST",
@@ -36,14 +36,13 @@ interface ApiService {
         @Query("pageNo") pageNo: Int = 1
     ): ApiResponse<TourDetailResponse>
 
-    @GET("searchKeyword1")
+    @GET("searchKeyword2")
     suspend fun getSearchKeyword(
         @Query("numOfRows") pageResultCount: Int = 20,
         @Query("pageNo") pageNo: Int = 1,
         @Query("MobileOS") os: String = "AND",
         @Query("MobileApp") mobileOs: String = "TEST",
         @Query("_type") type: String = "json",
-        @Query("listYN") listYN: String = "Y",
         @Query("arrange") arrange: String = "C",
         @Query("keyword") keyword: String,
         @Query("contentTypeId") contentTypeId: Int
