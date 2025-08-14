d4fdb2612f34c34f04a5efa29e50bae6a99e1162
idstudent <sm1374@naver.com>
2025-08-14 09:31:00 +0900

    버전올림

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.ljystamp.stamp_tour_app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0.6"
+        versionCode = 9
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         val properties = gradleLocalProperties(rootDir)
